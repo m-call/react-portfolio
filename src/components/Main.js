ea5599ef40d7f30282a62b08f4401316f915c042
Michael Callahan <mcallahan0@yahoo.com>
2021-08-23 15:35:10 -0400

    Added and implemented a Main component and css file. Added Main component to App.js

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Main.css'
+
+export default function Main() {
+
+    return (
+        <main className="wrapper">
+            
+        </main>
+    );
+};
